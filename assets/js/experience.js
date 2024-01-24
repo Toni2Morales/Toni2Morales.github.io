e9542e21ab4db073b6c3dc554b648bbac0392d91
Toni <107318721+Toni2Morales@users.noreply.github.com>
2024-01-24 23:09:05 +0100

    PortfolioModificado1

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -15,7 +15,7 @@ const exp = [
     title: "Data Scientist",
     cardImage: "assets/images/experience-page/GEODIS.jpg",
     place: "GEODIS",
-    time: "Oct 2022 - Dic 2022",
+    time: "Oct 2022 - Dic 2023",
     desp: "<li>Creación de modelos predictivos de IA</li> <li>Creación de APIs para conectar los resultados con otros sistemas</li> <li>Manupulación, extracción y transformación de datos</li>",
   },
 
